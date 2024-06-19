566d030df6523d27de1f309baff8c033d4caf79e
Jawad Sher <158135119+JawadSher@users.noreply.github.com>
2024-06-19 09:54:11 +0500

    Update main.cpp

--- 1 - Multiple Grid-Matrix Patterns with Loops/Pattern - 1/main.cpp
+++ 1 - Multiple Grid-Matrix Patterns with Loops/Pattern - 1/main.cpp
@@ -1,1 +1,25 @@
+// Simple grid with repeating sequences of numbers in each row.
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int N;
+    cout<<"Enter the value of N: ";
+    cin>>N;
+
+    int row= 1;
+    while(row <= N) {
+
+        int col = 1;
+        while(col <= N) {
+            cout<<col<<" ";
+            col++;
+        }
+        cout<<endl;
+        row++;
+    }
+
+    return 0;
+}
 
