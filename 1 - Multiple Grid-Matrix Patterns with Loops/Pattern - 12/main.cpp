4162937f06aeeb16cc67e4e52e6091c6e2a76b86
Jawad Sher <158135119+JawadSher@users.noreply.github.com>
2024-06-19 12:08:32 +0500

    Create main.cpp

---
+++ 1 - Multiple Grid-Matrix Patterns with Loops/Pattern - 12/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N;
+  cout << "Enter the value of N: ";
+  cin >> N;
+
+  int row = 1;
+  while (row <= N) {
+    int col = 1;
+    while (col <= row) {
+      char ch = 'A' + row - 1;
+      cout << ch << " ";
+      col++;
+    }
+    cout << endl;
+    row++;
+  }
+
+  return 0;
+}
