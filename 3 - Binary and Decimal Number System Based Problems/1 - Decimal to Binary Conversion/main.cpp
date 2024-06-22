15d2dcd2e6a2b85d3459f78cf8532d7e8184b481
Jawad Sher <158135119+JawadSher@users.noreply.github.com>
2024-06-22 15:01:31 +0500

    Update main.cpp

--- 3 - Binary and Decimal Number System Based Problems/1 - Decimal to Binary Conversion/main.cpp
+++ 3 - Binary and Decimal Number System Based Problems/1 - Decimal to Binary Conversion/main.cpp
@@ -1,1 +1,25 @@
+// Approach 1 : Division Method
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int decimal_to_binary(int n){
+    int binary = 0;
+    int i = 0;
+    while(n){
+        int bit = n & 1;
+        binary = (bit * pow(10, i)) + binary;
+        n = n >> 1;
+        i = i + 1;
+    }
+    return binary;
+}
+
+int main() {
+    int decimal = 13;
+    int output = decimal_to_binary(decimal);
+    cout<<"Binary of "<<decimal<<" is : "<<output;
+    return 0;
+}
+
 
