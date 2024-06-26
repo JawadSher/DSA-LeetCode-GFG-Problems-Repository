0c8fd6a6a1d18e1abc153d55df720a251864cffe
Jawad Sher <158135119+JawadSher@users.noreply.github.com>
2024-06-26 10:50:30 +0500

    Update main.cpp

--- 4 - Arrays Based Problems/Find Minimum & Maximum Value in Array/main.cpp
+++ 4 - Arrays Based Problems/Find Minimum & Maximum Value in Array/main.cpp
@@ -1,1 +1,36 @@
+// Linear Search Approach
+#include<iostream>
+using namespace std;
+
+int maxValue(int arr[], int n){
+    int value = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] > value){
+            value = arr[i];
+        }
+    }
+    return value;
+}
+
+int minValue(int arr[], int n){
+    int value = 0;
+    for(int i = 0; i < n; i++){
+        if(arr[i] < value){
+            value = arr[i];
+        }
+    }
+    return value;
+}
+
+int main(){
+    int my_arr[] = {10, 20, 30, -50, -30, 100, 1000};
+    int size = sizeof(my_arr)/sizeof(int);
+    
+    int max_val = maxValue(my_arr, size);
+    int min_val = minValue(my_arr, size);
+    cout<<"Max value is : "<<max_val<<endl;
+    cout<<"Min value is : "<<min_val;
+    return 0;
+}
+
 
