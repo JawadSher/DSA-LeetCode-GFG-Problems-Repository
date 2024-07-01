e938304ddca465ddd6750cc3ca8b547b36e21895
Jawad Sher <158135119+JawadSher@users.noreply.github.com>
2024-07-01 12:10:05 +0500

    Create main.cpp

---
+++ 5 - Binary Search Algorithm Based Problems/Return Last Occurence of Element in Array/main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int last_occurence(int arr[], int n, int k){
+    int start = 0;
+    int end = n - 1;
+    int mid = start + (end - start)/2;
+    int index = -1;
+    
+    while(start <= end){
+        if(arr[mid] == k){
+            index = mid;
+            start = mid + 1;
+        }else if(k < arr[mid]){
+            end = mid - 1;
+        }else{
+            start = mid +1;
+        }
+        mid = start + (end - start)/2;
+    }
+    return index;
+}
+
+int main(){
+    int new_array[] = {10, 20, 30, 30, 30, 40, 50, 60, 70};
+    int size = sizeof(new_array) / sizeof(int);
+    int key = 30;
+    
+    int occurence = last_occurence(new_array, size, key);
+    cout<<"The Last Occurence "<<occurence<<" of Value "<<key;
+    
+    return 0;
+}
