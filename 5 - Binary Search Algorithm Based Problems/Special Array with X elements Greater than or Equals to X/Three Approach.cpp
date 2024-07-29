95340c72d0ba8c3af52f38ca2fd0e2772e5524c4
Jawad Sher <158135119+JawadSher@users.noreply.github.com>
2024-07-29 09:15:28 +0500

    Create Three Approach.cpp

---
+++ 5 - Binary Search Algorithm Based Problems/Special Array with X elements Greater than or Equals to X/Three Approach.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+   
+    int specialArray(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        for(int x = 0; x <= n; x++){
+            int count = n - (lower_bound(nums.begin(), nums.end(), x) - nums.begin());
+            if(count == x){
+                return x;
+            }
+        }
+    return -1;
+    }
+};
