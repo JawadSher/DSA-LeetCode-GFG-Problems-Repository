3df3cb63e5cb3ec0ea3390f40110f6e787ab0fb2
Jawad Sher <158135119+JawadSher@users.noreply.github.com>
2024-09-05 14:43:10 +0500

    Create main.cpp

---
+++ 08 - Strings and Characters Problems/04 - Pallindrome String/main.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:	
+	
+	int isPalindrome(string S)
+	{
+	    int i = 0;
+	    int j = S.length() - 1;
+	    
+	    if(S.length() <= 0){
+	        return 1;
+	    }
+	    
+	    while(i < j){
+	        if(S[i] != S[j]){
+	            return 0;
+	        }
+	        i++;
+	        j--;
+	    }
+	    
+	    return 1;
+	}
+
+};
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
