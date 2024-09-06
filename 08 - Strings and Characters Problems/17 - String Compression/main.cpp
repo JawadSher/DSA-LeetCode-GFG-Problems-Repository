5fee1469d53c06039365929819e85d01bede1cfd
Jawad Sher <158135119+JawadSher@users.noreply.github.com>
2024-09-06 18:07:15 +0500

    Create main.cpp

---
+++ 08 - Strings and Characters Problems/17 - String Compression/main.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int n = chars.size();
+        int i = 0;
+        int index = 0;
+
+        while(i < n){
+            int j = i + 1;
+
+            while(j < n && chars[i] == chars[j]){
+                j++;
+            }
+
+            chars[index++] = chars[i];
+            
+            int count = j - i;
+
+            if(count > 1){
+                string s_count = to_string(count);
+                for(char c : s_count){
+                    chars[index++] = c;
+                }
+
+            }
+
+            i = j;
+        }
+        
+        return index;
+    }
+};
