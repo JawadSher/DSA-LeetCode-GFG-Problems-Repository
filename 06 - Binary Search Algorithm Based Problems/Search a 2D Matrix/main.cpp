7b8ade8d5d20f7157eae5fcb8f2b19859195a53b
Jawad Sher <158135119+JawadSher@users.noreply.github.com>
2024-09-09 10:41:11 +0500

    Create main.cpp

---
+++ 06 - Binary Search Algorithm Based Problems/Search a 2D Matrix/main.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        for(int i = 0; i < rows; i++){
+            int start = 0;
+            int end = cols-1;
+
+            while(start <= end){
+                int mid = start + (end - start)/2;
+
+                if(matrix[i][mid] == target){
+                   return true;
+                }else if(matrix[i][mid] > target){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+            }
+        }
+
+        return false;
+    }
+};
