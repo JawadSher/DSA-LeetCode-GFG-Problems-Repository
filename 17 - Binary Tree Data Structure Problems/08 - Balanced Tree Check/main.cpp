d1457dfd135bfa97ee849cc32e60648f899e8eeb
Jawad Sher <158135119+JawadSher@users.noreply.github.com>
2024-10-28 12:13:38 +0500

    Create main.cpp

---
+++ 17 - Binary Tree Data Structure Problems/08 - Balanced Tree Check/main.cpp
@@ -0,0 +1,21 @@
+class Solution{
+    public:
+    int heights(Node* root){
+        if(root == NULL) return 0;
+        
+        int left = heights(root -> left);
+        if(left == -1) return -1;
+        
+        int right = heights(root -> right);
+        if(left == -1) return -1;
+        
+        if(abs(left - right) > 1) return -1;
+        
+        return max(left, right) + 1;
+    }
+    bool isBalanced(Node *root)
+    {
+        return heights(root) != -1;
+
+    }
+};
