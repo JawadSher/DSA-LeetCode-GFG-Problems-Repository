f4c985a5a9ef780347a6e11b38d270ad2d1530ab
Jawad Sher <158135119+JawadSher@users.noreply.github.com>
2024-11-14 16:29:55 +0500

    Create Worst Approach.cpp

---
+++ 19 - Heap Data Structure Problems/11 - Kth Largest Sum Contiguous Subarray/Worst Approach.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    int kthLargest(vector<int> &arr, int k) {
+        vector<int> sumStore;
+        
+        int n = arr.size();
+        
+        for(int i = 0; i < n; i++){
+            int sum = 0;
+            
+            for(int j = i; j < n; j++){
+                sum += arr[j];
+                sumStore.push_back(sum);
+            }
+        }
+        
+        sort(sumStore.begin(), sumStore.end());
+        
+        return sumStore[sumStore.size()-k];
+    }
+};
