ad20963a4f5ad6b8e5d484208b87420c297d1dbc
Jawad Sher <158135119+JawadSher@users.noreply.github.com>
2024-12-03 12:27:20 +0500

    Update 01 - Recursive Approach.cpp

--- 24 - Dynamic Programming Problems/01 - Example | Nth Fibonacci Number/01 - Recursive Approach.cpp
+++ 24 - Dynamic Programming Problems/01 - Example | Nth Fibonacci Number/01 - Recursive Approach.cpp
@@ -1,8 +1,14 @@
 class Solution {
   public:
     int nthFibonacci(int n) {
-        if(n == 0 || n == 1) return n;
-        
-        return nthFibonacci(n-1) + nthFibonacci(n-2);
+        // Base case: If n is 0 or 1, return n directly.
+        // F(0) = 0 and F(1) = 1 are the first two terms of the Fibonacci sequence.
+        if (n == 0 || n == 1) 
+            return n;
+
+        // Recursive case:
+        // The nth Fibonacci number is the sum of the (n-1)th and (n-2)th Fibonacci numbers.
+        // Recursively calculate the values for smaller subproblems.
+        return nthFibonacci(n - 1) + nthFibonacci(n - 2);
     }
 };
